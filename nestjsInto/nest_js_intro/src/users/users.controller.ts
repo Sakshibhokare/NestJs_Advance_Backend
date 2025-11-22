dd6ddef15724a35b73b3bff472ef0aca89ae80a3
Sakshibhokare <bhokaresakshi93@gmail.com>
2025-11-22 15:50:26 +0530

    decorators

--- nestjsInto/nest_js_intro/src/users/users.controller.ts
+++ nestjsInto/nest_js_intro/src/users/users.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+//we have headers and Ip as well to get headers details and Ip address of sender 
 @Controller('users')
 export class UsersController {
     //using routing decorators
-    @Get()
-    public getUsers(){
+    @Get("/:id/{:optional}")
+    public getUsers(@Param() Param:any, @Query() query:any){
+        console.log(Param);
+        console.log("limit and offsetValues" + query)
         return "we are inside of getUsers";
     }
 
     @Post()
-    public postUsers(){
+    public postUsers(@Body() requestBody: any){
+        console.log(requestBody)
         return "we are inside of postUsers";
     }
 }
